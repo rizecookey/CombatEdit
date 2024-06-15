3728ae205e51774e6ca72af27ec68130359aa946
Jonas Mittnacht <jonas@rizecookey.net>
2024-06-15 21:05:18 +0200

    Suppress deprecation warning coming from Minecraft source

--- src/main/java/net/rizecookey/combatedit/mixins/compatibility/LevelPropertiesMixin.java
+++ src/main/java/net/rizecookey/combatedit/mixins/compatibility/LevelPropertiesMixin.java
@@ -11,6 +11,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+@SuppressWarnings("deprecation")
 @Mixin(LevelProperties.class)
 public class LevelPropertiesMixin {
     @Inject(method = "readProperties", at = @At("HEAD"))
