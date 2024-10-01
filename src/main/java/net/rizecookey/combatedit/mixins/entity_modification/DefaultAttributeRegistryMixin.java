953cc1939a2e8f7f0270dc649533789ca3927ee3
Jonas M. <38248521+rizecookey@users.noreply.github.com>
2024-10-01 15:16:18 +0200

    Likely better compatibility with custom entities

--- src/main/java/net/rizecookey/combatedit/mixins/entity_modification/DefaultAttributeRegistryMixin.java
+++ src/main/java/net/rizecookey/combatedit/mixins/entity_modification/DefaultAttributeRegistryMixin.java
@@ -1,16 +1,28 @@
 package net.rizecookey.combatedit.mixins.entity_modification;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.DefaultAttributeRegistry;
 import net.rizecookey.combatedit.extension.MutableDefaultAttributeContainer;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Mixin(DefaultAttributeRegistry.class)
 public class DefaultAttributeRegistryMixin {
-    @ModifyExpressionValue(method = "<clinit>", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/attribute/DefaultAttributeContainer$Builder;build()Lnet/minecraft/entity/attribute/DefaultAttributeContainer;"))
-    private static DefaultAttributeContainer replaceWithMutable(DefaultAttributeContainer value) {
-        return MutableDefaultAttributeContainer.copyOf(value);
+    @Shadow public static Map<EntityType<? extends LivingEntity>, DefaultAttributeContainer> DEFAULT_ATTRIBUTE_REGISTRY;
+
+    @Inject(method = "<clinit>", at = @At("TAIL"))
+    private static void replaceWithMutable(CallbackInfo ci) {
+        DEFAULT_ATTRIBUTE_REGISTRY = DEFAULT_ATTRIBUTE_REGISTRY.entrySet()
+                .stream()
+                .map(entry -> Map.entry(entry.getKey(), MutableDefaultAttributeContainer.copyOf(entry.getValue())))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 }
