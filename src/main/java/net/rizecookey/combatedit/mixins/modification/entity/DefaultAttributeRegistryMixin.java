800fb16dcdceb46c5beb86460f7b86c3dcd016a2
Jonas M. <38248521+rizecookey@users.noreply.github.com>
2024-10-05 14:50:54 +0200

    Add better compatibility with custom living entities

--- src/main/java/net/rizecookey/combatedit/mixins/modification/entity/DefaultAttributeRegistryMixin.java
+++ src/main/java/net/rizecookey/combatedit/mixins/modification/entity/DefaultAttributeRegistryMixin.java
@@ -1,31 +1,33 @@
 package net.rizecookey.combatedit.mixins.modification.entity;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.DefaultAttributeRegistry;
 import net.rizecookey.combatedit.extension.DynamicDefaultAttributeContainer;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Mixin(value = DefaultAttributeRegistry.class, priority = Integer.MAX_VALUE)
 public class DefaultAttributeRegistryMixin {
-    @Shadow @Final @Mutable
-    public static Map<EntityType<? extends LivingEntity>, DefaultAttributeContainer> DEFAULT_ATTRIBUTE_REGISTRY;
+    @Unique
+    private static Map<EntityType<? extends LivingEntity>, DynamicDefaultAttributeContainer> DYNAMIC_DEFAULT_ATTRIBUTE_REGISTRY;
 
     @Inject(method = "<clinit>", at = @At("TAIL"))
-    private static void replaceWithMutable(CallbackInfo ci) {
-        DEFAULT_ATTRIBUTE_REGISTRY = DEFAULT_ATTRIBUTE_REGISTRY.entrySet()
-                .stream()
-                .map(entry -> Map.entry(entry.getKey(), new DynamicDefaultAttributeContainer(entry.getValue())))
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    private static void initDynamicRegistry(CallbackInfo ci) {
+        DYNAMIC_DEFAULT_ATTRIBUTE_REGISTRY = new HashMap<>();
+    }
+
+    @ModifyReturnValue(method = "get", at = @At("RETURN"))
+    private static DefaultAttributeContainer useDynamic(DefaultAttributeContainer defaultAttributeContainer, @Local(argsOnly = true) EntityType<? extends LivingEntity> type) {
+        return DYNAMIC_DEFAULT_ATTRIBUTE_REGISTRY.computeIfAbsent(type, key -> new DynamicDefaultAttributeContainer(defaultAttributeContainer));
     }
 }
